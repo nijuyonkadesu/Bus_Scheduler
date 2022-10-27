9d79802bc882a92507d4a93cea7c3441462d3080
nijuyonkadesu <24ksriramv@gmail.com>
2022-10-27 18:38:08 +0530

    defining Entity and Dao

---
+++ app/src/main/java/com/example/busschedule/database/schedule/Schedule.kt
@@ -0,0 +1,24 @@
+package com.example.busschedule.database.schedule
+
+import androidx.room.ColumnInfo
+import androidx.room.Dao
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.Query
+
+@Entity
+data class Schedule(
+    @PrimaryKey val id: Int,
+    @ColumnInfo(name = "stop_name") val stopName: String,
+    @ColumnInfo(name = "arrival_time") val arrivalTime: Int,
+)
+// @NotNull is equivalent to not having '?' in property
+
+@Dao
+interface ScheduleDao {
+    @Query("select * from schedule order by arrival_time")
+    fun getAll(): List<Schedule> // For screen 1
+
+    @Query("select * from schedule where stop_name = :stopName order by arrival_time")
+    fun getByStopName(): List<Schedule>
+}
